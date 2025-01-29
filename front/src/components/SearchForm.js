70d7f9fef451446720dddaf9041e94505f738b74
fefe <humeauf@yahoo.com>
2025-01-29 17:32:22 +0100

    HomePage + Navbar

--- front/src/components/SearchForm.js
+++ front/src/components/SearchForm.js
@@ -1,37 +1,69 @@
-import React from 'react';
-import map from '../assets/Carte.png';
-import calender from '../assets/Calendrier.png';
-import people from '../assets/personne.png';
+import React, { forwardRef, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"; // Style par défaut du datepicker
+
 import { useNavigate } from "react-router-dom";
+import calender from "../assets/Calendrier.png";
+import map from "../assets/Carte.png";
+import TravelersCounter from "./TravelersCounter"; // Import du composant TravelersCounter
 
 function SearchForm() {
   const navigate = useNavigate();
-  return (
-      <div className="search-form">
-          <div className="inputs-container">
-              <div className="input-container">
-                  <input type="text" placeholder="Où allez-vous?" className="search-input" />
-                  <div className="icon-container">
-                    <img src={map} alt={'date'} className="icon"/>
-                  </div>
-              </div>
-              <div className="input-container">
-                  <input type="text" placeholder="Quand partez-vous?" className="search-input"/>
-                  <div className="icon-container">
-                    <img src={calender} alt={'date'} className="icon"/>
-                  </div>
-              </div>
-              <div className="input-container">
-                  <input type="text" placeholder="À combien de voyageurs?" className="search-input" />
-                  <div className="icon-container">
-                   <img src={people} alt={'people'} className="icon"/>
-                  </div>
-              </div>
-          </div>
-          <button className="search-button" onClick={() => navigate("/about")}>
-            Rechercher
-          </button>
+
+  const [dateRange, setDateRange] = useState([null, null]); // Tableau contenant [startDate, endDate]
+  const [startDate, endDate] = dateRange; // Déstructure la plage en startDate et endDate
+
+  // Composant personnalisé pour rendre le champ "lecture seule"
+  const CustomDateInput = forwardRef(({ value, onClick }, ref) => (
+    <div onClick={onClick} ref={ref} className="search-input">
+      {value || "Dates de voyage"} {/* Texte par défaut ou les dates sélectionnées */}
+      <div className="icon-container">
+        <img src={calender} alt="calendar" className="icon" />
       </div>
+    </div>
+  ));
+
+  return (
+    <div className="search-form">
+      <div className="inputs-container">
+        {/* Champ pour la destination */}
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Où allez-vous?"
+            className="search-input"
+          />
+          <div className="icon-container">
+            <img src={map} alt={"map"} className="icon" />
+          </div>
+        </div>
+
+        {/* Champ pour la plage de dates */}
+        <div className="input-container">
+          <DatePicker
+            selected={startDate}
+            onChange={(update) => setDateRange(update)}
+            startDate={startDate}
+            endDate={endDate}
+            selectsRange
+            placeholderText="Dates de voyage"
+            customInput={<CustomDateInput />} // Utilisation de l'input personnalisé
+            dateFormat="dd/MM/yyyy"
+            isClearable
+          />
+        </div>
+
+        {/* Remplacement du champ pour les voyageurs */}
+        <div className="input-container">
+          <TravelersCounter /> {/* Intégration du composant TravelersCounter */}
+        </div>
+      </div>
+
+      {/* Bouton de recherche */}
+      <button className="search-button" onClick={() => navigate("/about")}>
+        Rechercher
+      </button>
+    </div>
   );
 }
 
