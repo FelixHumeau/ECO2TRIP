e9a109e8250ad8152e25971828b0d8b2dea44baf
stina-houngavou-agrikola-ai <stina.houngavou@agrikola.ai>
2025-01-29 15:00:03 +0100

    feat: Ajout du carousel d'images

--- front/src/pages/SummaryPage.js
+++ front/src/pages/SummaryPage.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import Carousel from "../components/Carousel";
 
 const SummaryPage = () => {
-  const navigate = useNavigate();
+  const images = [
+    "https://www.ot-lelavandou.fr/app/uploads/2023/07/port-cros-hyeres-a-tourisme-provence-mediterranee-julien-mauceri.webp",
+    "https://www.espritparcnational.com/sites/default/files/2020-10/PNPC_006193Ori.jpg",
+    "https://res.cloudinary.com/manawa/image/private/f_auto,c_limit,w_3840,q_auto/85247e2c250c6c1b9c94a3f96497131c",
+    "https://www.rivieraloisirs.com/public/uploads/2020/06/iStock-536012111.jpg",
+  ];
 
   return (
-    <div>
-      <h1>Résumé de votre voyage</h1>
-      <button onClick={() => navigate("/contact")}>
-        Retour à l'accueil
-      </button>
+    <div style={{ textAlign: "center" }}>
+      <Carousel images={images} height="700px" width="90%" />
     </div>
   );
 };
