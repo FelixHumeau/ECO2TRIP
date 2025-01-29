4b41ef4e818c67a8f97f8ba6d9ed8beb1aca9fdc
stina-houngavou-agrikola-ai <stina.houngavou@agrikola.ai>
2025-01-29 15:03:31 +0100

    feat: Ajout de la carte ville et du modal Localisation

--- front/src/pages/SummaryPage.js
+++ front/src/pages/SummaryPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Carousel from "../components/Carousel";
+import CityCard from "../components/CityCard";
 
 const SummaryPage = () => {
   const images = [
@@ -9,9 +10,24 @@ const SummaryPage = () => {
     "https://www.rivieraloisirs.com/public/uploads/2020/06/iStock-536012111.jpg",
   ];
 
+  const cityData = {
+    cityName: "Île de Port Cros",
+    rating: 4.6,
+    tags: ["Mer", "Aquatique", "Parc naturel"," Plongée", "Randonée"],
+    carbonFootprint: 280,
+    coordinates: [43.002,6.399],
+  };
+
   return (
     <div style={{ textAlign: "center" }}>
       <Carousel images={images} height="700px" width="90%" />
+      <CityCard
+        cityName={cityData.cityName}
+        rating={cityData.rating}
+        tags={cityData.tags}
+        carbonFootprint={cityData.carbonFootprint}
+        coordinates={cityData.coordinates}
+      />
     </div>
   );
 };
