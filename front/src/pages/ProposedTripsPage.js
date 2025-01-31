6142876a378c3a5f203918c106c2ae7c1b5ae99e
tom <tombernarg@gmail.com>
2025-01-31 13:18:47 +0000

    cities

--- front/src/pages/ProposedTripsPage.js
+++ front/src/pages/ProposedTripsPage.js
@@ -5,10 +5,22 @@ import ExpandableMapCard from "../components/ExpandableCard";
 
 const ProposedTripsPage = () => {
   const cityData = {
-    city: "Marseille",
+    city: "Ile de Port-Cros",
     description: "Une belle ville portuaire avec une riche histoire. Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla",
     imageSrc: "https://www.ot-lelavandou.fr/app/uploads/2023/07/port-cros-hyeres-a-tourisme-provence-mediterranee-julien-mauceri.webp",
     tags: ["Mer", "Soleil", "Culture"],
+    carbonFootprint: {
+      transport: 100,  // en kg CO₂
+      housing: 50,    // en kg CO₂
+      activities: 10, // en kg CO₂
+    },
+    days: 5, // Nombre de jours du séjour
+  };
+  const city2data = {
+    city: "Aix-les-bains",
+    description: "Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla",
+    imageSrc: "https://www.espritparcnational.com/sites/default/files/2020-10/PNPC_006193Ori.jpg",
+    tags: ["Mer", "Surf"],
     carbonFootprint: {
       transport: 300,  // en kg CO₂
       housing: 30,    // en kg CO₂
@@ -35,24 +47,15 @@ const ProposedTripsPage = () => {
         tags={cityData.tags}
         carbonFootprint={cityData.carbonFootprint}
         days={cityData.days}
-        price= "300-540€"
+        price= "340-610€"
       />
             <ExpandableMapCard
-        city={cityData.city}
-        description={cityData.description}
-        imageSrc={cityData.imageSrc}
-        tags={cityData.tags}
-        carbonFootprint={cityData.carbonFootprint}
-        days={cityData.days}
-        price= "300-540€"
-      />
-            <ExpandableMapCard
-        city={cityData.city}
-        description={cityData.description}
-        imageSrc={cityData.imageSrc}
-        tags={cityData.tags}
-        carbonFootprint={cityData.carbonFootprint}
-        days={cityData.days}
+        city={city2data.city}
+        description={city2data.description}
+        imageSrc={city2data.imageSrc}
+        tags={city2data.tags}
+        carbonFootprint={city2data.carbonFootprint}
+        days={city2data.days}
         price= "300-540€"
       />
     </div>
